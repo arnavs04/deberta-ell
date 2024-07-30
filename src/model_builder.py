bffba05efed5552b13b553d77ebfd3e16fd2d3fd
Arnav Samal <samalarnav@gmail.com>
2024-07-31 04:40:44 +0530

    organising and cleaning

--- src/model_builder.py
+++ src/model_builder.py
@@ -3,49 +3,41 @@ from torch import nn
 from transformers import AutoConfig, AutoModel
 from utils import *
 
-LOGGER = get_logger()
+# LOGGER = get_logger()
 
 
 class MeanPooling(nn.Module):
     def __init__(self):
         super(MeanPooling, self).__init__()
-    
+
     def forward(self, last_hidden_state, attention_mask):
-        input_mask_expanded = attention_mask.unsqueeze(-1).expand(
-            last_hidden_state.size()).float()
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(last_hidden_state.size()).float()
         sum_embeddings = torch.sum(last_hidden_state * input_mask_expanded, 1)
-        sum_mask = input_mask_expanded.sum(1)
-        sum_mask = torch.clamp(sum_mask, min=1e-9)
-        mean_embeddings = sum_embeddings / sum_mask
-        return mean_embeddings
+        sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+        return sum_embeddings / sum_mask
 
 
 class CustomModel(nn.Module):
     def __init__(self, cfg, config_path=None, pretrained=False):
         super().__init__()
         self.cfg = cfg
-        if config_path is None:
-            self.config = AutoConfig.from_pretrained(cfg.model, output_hidden_states=True)
-            self.config.hidden_dropout = 0.
-            self.config.hidden_dropout_prob = 0.
-            self.config.attention_dropout = 0.
-            self.config.attention_probs_dropout_prob = 0.
-            LOGGER.info(self.config)
-        else:
-            self.config = torch.load(config_path)
+        self.config = AutoConfig.from_pretrained(cfg.model, output_hidden_states=True) if config_path is None else torch.load(config_path)
+        if not config_path:
+            self.config.update({
+                "hidden_dropout": 0.0,
+                "hidden_dropout_prob": 0.0,
+                "attention_dropout": 0.0,
+                "attention_probs_dropout_prob": 0.0
+            })
         
-        if pretrained:
-            self.model = AutoModel.from_pretrained(cfg.model, config=self.config)
-        else:
-            self.model = AutoModel(self.config)
-        
-        if self.cfg.gradient_checkpointing:
+        self.model = AutoModel.from_pretrained(cfg.model, config=self.config) if pretrained else AutoModel(self.config)
+        if cfg.gradient_checkpointing:
             self.model.gradient_checkpointing_enable()
         
         self.pool = MeanPooling()
         self.fc = nn.Linear(self.config.hidden_size, 6)
         self._init_weights(self.fc)
-    
+
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
             module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
@@ -58,17 +50,13 @@ class CustomModel(nn.Module):
         elif isinstance(module, nn.LayerNorm):
             module.bias.data.zero_()
             module.weight.data.fill_(1.0)
-    
+
     def feature(self, inputs):
-        outputs = self.model(**inputs)
-        last_hidden_states = outputs[0]
-        feature = self.pool(last_hidden_states, inputs['attention_mask'])
-        return feature
-    
+        last_hidden_states = self.model(**inputs)[0]
+        return self.pool(last_hidden_states, inputs['attention_mask'])
+
     def forward(self, inputs):
-        feature = self.feature(inputs)
-        output = self.fc(feature)
-        return output
+        return self.fc(self.feature(inputs))
 
     # def forward(self, input_ids, attention_mask):
     #     # for checking model complexity and size
